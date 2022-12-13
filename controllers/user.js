4f7f993fc5f386042b46c0bd4d7784f715e2a74f
Paloma Borro <paloma.borro@wizeline.com>
2022-12-13 15:13:49 +0100

    Add uploadImage, remove test route and image

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,8 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("../services/auth-service");
-
-const test = (req, res) => {
-  res.status(200).send({ message: "Test" });
-};
+const fs = require("fs");
+const path = require("path");
 
 const saveUser = (req, res) => {
   const user = new User();
@@ -88,4 +86,58 @@ const updateUser = (req, res) => {
   });
 };
 
-module.exports = { test, saveUser, loginUser, updateUser };
+const uploadImage = (req, res) => {
+  const userId = req.params.id;
+  let file_name = "Not uploaded...";
+
+  if (req.files) {
+    const file_path = req.files.image.path;
+    const file_split = file_path.toString().split("/");
+    const file_name = file_split[2];
+
+    const ext_split = file_name.split(".");
+    const file_ext = ext_split[1] || null;
+
+    if (
+      file_ext === "png" ||
+      file_ext === "jpg" ||
+      file_ext === "jpeg" ||
+      file_ext === "gif"
+    ) {
+      User.findByIdAndUpdate(
+        userId,
+        { image: file_name },
+        (err, userUpdated) => {
+          if (err) {
+            res.status(404).send({ message: "User not updated" });
+          } else {
+            res.status(200).send({ image: file_name, user: userUpdated });
+          }
+        }
+      );
+    } else {
+      res.status(200).send({ message: "Invalid extension" });
+    }
+  } else {
+    res.status(200).send({ message: "You have not uploaded any image" });
+  }
+};
+
+const getImageFile = (req, res) => {
+  const imageFile = req.params.imageFile;
+  const path_file = `./uploads/users/${imageFile}`;
+
+  if (fs.existsSync(path_file)) {
+    res.sendFile(path.resolve(path_file));
+  } else {
+    res.status(404).send({ message: "Image does not exist" });
+  }
+};
+
+module.exports = {
+  saveUser,
+  loginUser,
+  updateUser,
+  uploadImage,
+  getImageFile,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const UserController = require("../controllers/user");
+const middlewareAuth = require("../middlewares/authentication");
+const multipart = require("connect-multiparty");
 
 const api = express.Router();
-const middlewareAuth = require("../middlewares/authentication");
 
-api.get("/test", middlewareAuth.isAuthenticated, UserController.test);
+const middlewareUpload = multipart({ uploadDir: "./uploads/users" });
+
+// User routes
+api.get("/get-image-user/:imageFile", UserController.getImageFile);
 
 api.post("/register", UserController.saveUser);
 api.post("/login", UserController.loginUser);
+api.post(
+  "/upload-image-user/:id",
+  [middlewareAuth.isAuthenticated, middlewareUpload],
+  UserController.uploadImage
+);
+
 api.put(
   "/update-user/:id",
   middlewareAuth.isAuthenticated,
