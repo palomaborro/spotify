9f197b2d5d8842eea9a3bf0816fa3e72841b9279
Paloma <palomabs93@gmail.com>
2023-09-06 15:21:09 +0200

    Fix favorite routes

--- api/routes/favorites.js
+++ api/routes/favorites.js
@@ -4,16 +4,16 @@ const middlewareAuth = require("../middlewares/authentication");
 
 const api = express.Router();
 
-api.post(
-  "/favorites/:id",
-  middlewareAuth.isAuthenticated,
-  FavoritesController.saveFavoriteSongs
-);
-
 api.get(
   "/favorites/:id",
   middlewareAuth.isAuthenticated,
   FavoritesController.getFavoriteSongs
+);
+
+api.post(
+  "/favorites/:id",
+  middlewareAuth.isAuthenticated,
+  FavoritesController.saveFavoriteSongs
 );
 
 module.exports = api;
